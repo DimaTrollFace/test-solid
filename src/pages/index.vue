5e3b85180ae7016f3d232f6fe70823391d972f5d
dimanevolnichenko@gmail.com <dimanevolnichenko@gmail.com>
2019-07-31 23:05:24 +0300

    init home page

---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+        <div class="home__text">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+    .home{
+        width: 100vw;
+        height: 100vh;
+        padding: 50px;
+    }
+</style>
